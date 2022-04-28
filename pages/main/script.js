b15b944be5d2b3b9387cc30ddf599aac4feb6437
jaba777 <tskhovreba777@gmail.com>
2022-04-29 01:57:55 +0400

    add section

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,6 +1,7 @@
 const menuSpan=document.querySelector('.menuspan');
 const menuContainer=document.querySelector('.menu-container');
 const House=document.querySelector('.house');
+
 menuSpan.addEventListener('click',()=>{
  House.classList.add('actives');
  menuContainer.classList.toggle('active');
