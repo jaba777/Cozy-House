0db1ee635d7404a7358726e6b0a834ec3ae7921c
jaba777 <tskhovreba777@gmail.com>
2022-04-29 20:56:53 +0400

    build js

--- pages/main/script.js
+++ pages/main/script.js
@@ -6,3 +6,31 @@ menuSpan.addEventListener('click',()=>{
  House.classList.add('actives');
  menuContainer.classList.toggle('active');
 })
+
+const imageBox=document.getElementsByClassName('imagepet-box');
+const ourpetsImage=document.querySelector('.ourPets-images');
+
+let counter=0;
+
+const rightIcon=document.querySelector('.rightIcon');
+const leftIcon=document.querySelector('.leftIcon');
+leftIcon.addEventListener('click',()=>{
+  if(counter == 0){
+    counter=imageBox.length-2;
+}
+  if(counter!==0){
+      counter--;
+  } 
+  ourpetsImage.style.transform= 'translate('+(counter) *  -16.5 +'%)';
+})
+
+
+rightIcon.addEventListener('click',()=>{
+  if(counter == imageBox.length-3){
+    counter=-1;
+}
+  if(counter<=imageBox.length-4){
+      counter++;
+  } 
+  ourpetsImage.style.transform= 'translate('+(counter) *  -16.5 +'%)';
+})
